8c6748e7ac885aa074540ec2d0f1b0a861b89b15
WolframRhodium <WolframRhodium@users.noreply.github.com>
2016-05-25 13:22:15 +0800

    AAMerge: Variable renames and bug fixes

--- muvsfunc.py
+++ muvsfunc.py
@@ -1,7 +1,7 @@
 import vapoursynth as vs
 import havsfunc as haf
 
-def AAMerge(Bsrc, aa_h, aa_v, rad=0, power=1.0, show=0):
+def AAMerge(Bsrc, aa_h, aa_v, mrad=0, power=1.0, show=0):
     core = vs.get_core()
     funcName = 'AAMerge'
     
@@ -11,47 +11,44 @@ def AAMerge(Bsrc, aa_h, aa_v, rad=0, power=1.0, show=0):
     if not isinstance(aa_h, vs.VideoNode):
         raise TypeError(funcName + ': \"aa_h\" must be a clip!')
     if Bsrc.format.id != aa_h.format.id:
-            raise ValueError(funcName + ': clip \"aa_h\" must be of the same format as the Bsrc clip!')
+        raise ValueError(funcName + ': clip \"aa_h\" must be of the same format as the Bsrc clip!')
     if Bsrc.width != aa_h.width or Bsrc.height != aa_h.height:
         raise ValueError(funcName + ': clip \"aa_h\" must be of the same size as the Bsrc clip!')
     
     if not isinstance(aa_v, vs.VideoNode):
         raise TypeError(funcName + ': \"aa_v\" must be a clip!')
     if Bsrc.format.id != aa_v.format.id:
-            raise ValueError(funcName + ': clip \"aa_v\" must be of the same format as the Bsrc clip!')
+        raise ValueError(funcName + ': clip \"aa_v\" must be of the same format as the Bsrc clip!')
     if Bsrc.width != aa_v.width or Bsrc.height != aa_v.height:
         raise ValueError(funcName + ': clip \"aa_v\" must be of the same size as the Bsrc clip!')   
         
-    if not isinstance(rad, int):
-        raise TypeError(funcName + '\"rad\" must be an int!')
+    if not isinstance(mrad, int):
+        raise TypeError(funcName + '\"mrad\" must be an int!')
     
     if not isinstance(power, float):
         raise TypeError(funcName + '\"power\" must be a float!')
 
     if not isinstance(show, int):
         raise TypeError(funcName + '\"show\" must be an int!')
-     
-    h_matrix = [-1, 2, -1, -1, 2, -1, -1, 2, -1]  
-    v_matrix = [-1, -1, -1, 2, 2, 2, -1, -1, -1]  
     
-    hmap = core.std.Convolution(Bsrc, h_matrix, saturate=False)
-    vmap = core.std.Convolution(Bsrc, v_matrix, saturate=False)
-    if rad > 0:
-        hmap = haf.mt_expand_multi(hmap, sw=rad, sh=0)
-        vmap = haf.mt_expand_multi(vmap, sw=0, sh=rad)
-    elif rad < 0:
-        hmap = haf.mt_inpand_multi(hmap, sw=-rad, sh=0)
-        vmap = haf.mt_inpand_multi(vmap, sw=0, sh=-rad)  
+    hmap = core.std.Convolution(Bsrc, matrix=[-1, 2, -1, -1, 2, -1, -1, 2, -1], saturate=False)
+    vmap = core.std.Convolution(Bsrc, matrix=[-1, -1, -1, 2, 2, 2, -1, -1, -1], saturate=False)
+    if mrad > 0:
+        hmap = haf.mt_expand_multi(hmap, sw=mrad, sh=0)
+        vmap = haf.mt_expand_multi(vmap, sw=0, sh=mrad)
+    elif mrad < 0:
+        hmap = haf.mt_inpand_multi(hmap, sw=-mrad, sh=0)
+        vmap = haf.mt_inpand_multi(vmap, sw=0, sh=-mrad)  
     
     bits = Bsrc.format.bits_per_sample
     ldexpr = '{peak} 1 y x / {power} pow + /'.format(peak=(1 << bits) - 1, power=power)
     ldmap = core.std.Expr([hmap, vmap], [ldexpr])
-    
-    if show == 0:
-        return core.std.MaskedMerge(aa_h, aa_v, ldmap)
-    elif show == 1:
+        
+    if show == 1:
         return ldmap
     elif show == 2:
         return hmap
     elif show == 3:
         return vmap
+    else:
+        return core.std.MaskedMerge(aa_h, aa_v, ldmap)
