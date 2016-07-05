f2b7b44d3156d2861c0df9ad4b2d4affdbcf55ed
WolframRhodium <WolframRhodium@users.noreply.github.com>
2016-07-05 14:36:48 +0800

    Compare(): Bug fixes and ...

--- muvsfunc.py
+++ muvsfunc.py
@@ -67,6 +67,8 @@ def Compare(src, flt, power=1.5, chroma=False, mode=1):
 
     if not isinstance(src, vs.VideoNode):
         raise TypeError(funcName + ': \"src\" must be a clip!')
+    if src.format.color_family not in [vs.GRAY, vs.YUV, vs.YCOCG]:
+    	raise TypeError(funcName + ': \"src\" must be a YUV clip!')
     if not isinstance(flt, vs.VideoNode):
         raise TypeError(funcName + ': \"flt\" must be a clip!')
     if src.format.id != flt.format.id:
@@ -74,23 +76,22 @@ def Compare(src, flt, power=1.5, chroma=False, mode=1):
     if mode not in [1, 2]:
         raise ValueError(funcName + ': \"mode\" must be in [1, 2]!')
 
-
     isGray = src.format.color_family == vs.GRAY
     bits = src.format.bits_per_sample
 
     expr = {}
     expr[1] = 'y x - abs 1 + {power} pow 1 -'.format(power=power)
-    expr[2] = 'y x - {scale} * {neutral} +'.format(scale=(1 << (16 - 1)) / ((1 << 16) ** (1 / power) - 1), neutral=1 << (16 - 1))
+    expr[2] = 'y x - {scale} * {neutral} +'.format(scale=32768 / (65536 ** (1 / power) - 1), neutral=32768)
 
     chroma = chroma or isGray
 
     if bits < 16:
         src = core.fmtc.bitdepth(src, bits=16)
         flt = core.fmtc.bitdepth(flt, bits=16)
-        diff = core.std.Expr([src, flt], [expr[mode]] if chroma else [expr[mode], '{neutral}'.format(neutral=1 << (16 - 1))])
+        diff = core.std.Expr([src, flt], [expr[mode]] if chroma else [expr[mode], '{neutral}'.format(neutral=32768)])
         diff = core.fmtc.bitdepth(diff, bits=bits, dmode=1)
     else:
-        diff = core.std.Expr([src, flt], [expr[mode]] if chroma else [expr[mode], '{neutral}'.format(neutral=1 << (16 - 1))])
+        diff = core.std.Expr([src, flt], [expr[mode]] if chroma else [expr[mode], '{neutral}'.format(neutral=32768)])
 
     return diff
 
