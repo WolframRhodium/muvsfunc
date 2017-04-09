a198c09c39302a998137e379f9ff5f39176caed6
WolframRhodium <842960624@qq.com>
2017-04-09 18:35:46 +0800

    Add LUM class filters
    Implementation of "LUM filters: a class of rank-order-based filters for
    smoothing and sharpening" (
    http://ieeexplore.ieee.org/abstract/document/205713/ )
    Too tired to fulfill the code over three weeks.

---
+++ Collections/LUM.py
@@ -0,0 +1,37 @@
+def lum_smoother(input, k):
+    low = muf.Sort(input, k, mode='min')
+    max = muf.Sort(input, k, mode='max')
+
+    return haf.Clamp(input, max, min)  # or just core.rgvs.RemoveGrain(input, k - 1)
+
+def lum_sharper(input, l=2):
+    if l not in range(1, 6):
+        raise ValueError("\'l\' must be in [1, 5] !")
+
+    low1 = muf.Sort(input, l, mode='min')
+    high1 = muf.Sort(input, l, mode='max')
+
+    return core.std.Expr([input, low1, high1], ['x y z + 2 / <= x y min x z max ?'])
+
+def lum_filter(input, k=3, l=4):
+    if (not isinstance(l, int)) or (not isinstance(l, int)) or (l < k):
+        raise ValueError("\'k\' and \'l\' must be in [1, 5] and \'k\' must be not greater than \'l\'!")
+
+    low_k = muf.Sort(input, k, mode='min')
+    low_l = muf.Sort(input, l, mode='min')
+    high_l = muf.Sort(input, l, mode='max')
+    high_k = muf.Sort(input, k, mode='max')
+
+    return core.std.Expr([input, low_k, low_l, high_l, high_k], ['x z a + 2 / <= x y < y x z min ? x b > b x a max ? ?', ''])
+
+def asymmetric_lum_filter(input, k=3, l=4, q=6, r=7):
+    if (not isinstance(k, int)) or (not isinstance(l, int)) or (not isinstance(q, int)) or (not isinstance(r, int)) or (not 1 <= k <= l <= q <= r <= 9):
+        raise ValueError("\'k\', \'l\', \'q\' and \'r\' must be in [1, 9] in increasing order!")
+
+    order_k = muf.Sort(input, k, mode='min')
+    order_l = muf.Sort(input, l, mode='min')
+    order_q = muf.Sort(input, q, mode='min')
+    order_r = muf.Sort(input, r, mode='min')
+
+    return core.std.Expr([input, order_k, order_l, order_q, order_r], 
+        ['x x y > x z min x z max ? x a > x b min x a max ? + 2 / <= x y > x z min x z max ? x a > x b min x a max ? ?'])
