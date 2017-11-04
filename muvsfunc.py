dfb0a23843980798a3bdbad9f7f429a3d498af3f
WolframRhodium <WolframRhodium@users.noreply.github.com>
2017-11-04 20:55:48 +0800

    Add SSIM()

--- muvsfunc.py
+++ muvsfunc.py
@@ -39,6 +39,7 @@ Functions:
     DisplayHistogram
     GuidedFilter (Color)
     GMSD
+    SSIM
 '''
 
 import vapoursynth as vs
@@ -3207,7 +3208,7 @@ def GuidedFilterColor(input, guidance, radius=4, regulation=0.01, use_gauss=Fals
 
 
 def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **depth_args):
-    """Gradient Magnitude Similarity Deviation Calculator
+    """Gradient Magnitude Similarity Deviation calculator
 
     GMSD is a new effective and efficient image quality assessment (IQA) model, which utilizes the pixel-wise gradient magnitude similarity (GMS) 
     between the reference and distorted images combined with standard deviation of the GMS map to predict perceptual image quality.
@@ -3218,7 +3219,7 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
     The lowerer the GMSD score, the higher the image perceptual quality.
     If "clip1" == "clip2", GMSD = 0.
 
-    All the internal calculations are done at 32-bit float.
+    All the internal calculations are done at 32-bit float, only one channel of the image will be processed.
 
     Args:
         clip1: The distorted clip, will be copied to output if "show_map" is False.
@@ -3227,11 +3228,11 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
 
         plane: (int) Specify which plane to be processed. Default is None.
 
-        downsample: (bool) Whether to averaged the clips over local 2x2 window and downsample by a factor of 2 before the MSSIM value is calculated.
+        downsample: (bool) Whether to average the clips over local 2x2 window and downsample by a factor of 2 before calculation.
             Default is True.
 
         c: (float) A positive constant that supplies numerical stability.
-            According to the paper, for all the databases, GMSD shows similar preference to the value of c.
+            According to the paper, for all the test databases, GMSD shows similar preference to the value of c.
             Default is 0.0026.
 
         show_map: (bool) Whether to return GMS map. If not, "clip1" will be returned. Default is False.
@@ -3261,7 +3262,7 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
     # Store the "clip1"
     clip1_src = clip1
 
-    # Convert to double type grayscale image
+    # Convert to float type grayscale image
     clip1 = mvf.GetPlane(clip1, plane)
     clip2 = mvf.GetPlane(clip2, plane)
     clip1 = mvf.Depth(clip1, depth=32, sample=vs.FLOAT, **depth_args)
@@ -3284,10 +3285,7 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
     # Compute the gradient magnitude similarity (GMS) map
     quality_map = core.std.Expr([clip1_grad_squared, clip2_grad_squared], ['2 x y * sqrt * {c} + x y + {c} + /'.format(c=c)])
 
-    # The following code is modified from mvf.PlaneStatistics(), which is used to compute the standard derivation of the GMS map as GMSD
-    floatFormat = core.register_format(vs.GRAY, vs.FLOAT, 32, 0, 0)
-    floatBlk = core.std.BlankClip(quality_map, format=floatFormat.id)
-    
+    # The following code is modified from mvf.PlaneStatistics(), which is used to compute the standard deviation of the GMS map as GMSD
     if core.std.get_functions().__contains__('PlaneStats'):
         map_mean = core.std.PlaneStats(quality_map, plane=[0], prop='PlaneStats')
     else:
@@ -3297,7 +3295,7 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
         mean = f.props.PlaneStatsAverage
         expr = "x {mean} - dup *".format(mean=mean)
         return core.std.Expr(clip, expr, floatFormat.id)
-    SDclip = core.std.FrameEval(floatBlk, functools.partial(_PlaneSDFrame, clip=quality_map), map_mean)
+    SDclip = core.std.FrameEval(quality_map, functools.partial(_PlaneSDFrame, clip=quality_map), map_mean)
 
     if core.std.get_functions().__contains__('PlaneStats'):
         SDclip = core.std.PlaneStats(SDclip, plane=[0], prop='PlaneStats')
@@ -3310,6 +3308,131 @@ def GMSD(clip1, clip2, plane=None, downsample=True, c=0.0026, show_map=False, **
         return fout
     output_clip = quality_map if show_map else clip1_src
     output_clip = core.std.ModifyFrame(output_clip, [output_clip, SDclip], selector=_PlaneGMSDTransfer)
+
+    return output_clip
+
+
+def SSIM(clip1, clip2, plane=None, downsample=True, k1=0.01, k2=0.03, fun=None, dynamic_range=1, show_map=False, **depth_args):
+    """Structural SIMilarity Index calculator
+
+    The Structural SIMilarity (SSIM) index is a method for measuring the similarity between two images.
+    It is based on the hypothesis that the HVS is highly adapted for extracting structural information, 
+    which compares local patterns of pixel intensities that have been normalized for luminance and contrast.
+
+    The mean SSIM (MSSIM) index value of the distorted image will be stored as frame property 'PlaneSSIM' in the output clip.
+
+    The value of SSIM measures the structural similarity in an image.
+    The higher the SSIM score, the higher the image perceptual quality.
+    If "clip1" == "clip2", SSIM = 1.
+
+    All the internal calculations are done at 32-bit float, only one channel of the image will be processed.
+
+    Args:
+        clip1: The distorted clip, will be copied to output if "show_map" is False.
+
+        clip2: Reference clip, must be of the same format and dimension as the "clip1".
+
+        plane: (int) Specify which plane to be processed. Default is None.
+
+        downsample: (bool) Whether to average the clips over local 2x2 window and downsample by a factor of 2 before calculation.
+            Default is True.
+
+        k1, k2: (int) Constants in the SSIM index formula. 
+            According to the paper, the performance of the SSIM index algorithm is fairly insensitive to variations of these values.
+            Default are 0.01 and 0.03.
+
+        fun: (function or float) The function of how the clips are filtered.
+            If it is None, it will be set to a gaussian filter whose standard deviation is 1.5. Note that the size of gaussian kernel is different from the one in MATLAB.
+            If it is a float, it specifies the standard deviation of the gaussian filter. (sigma in core.tcanny.TCanny)
+            According to the paper, the quality map calculated from gaussian filter exhibits a locally isotropic property,
+            which prevents the present of undesirable “blocking” artifacts in the resulting SSIM index map.
+            Default is None.
+
+        dynamic_range: (float) Dynamic range of the internal float point clip. Default is 1.
+
+        show_map: (bool) Whether to return SSIM index map. If not, "clip1" will be returned. Default is False.
+
+        depth_args: (dict) Additional arguments passed to mvf.Depth() in the form of keyword arguments.
+            Default is {}.
+
+    Ref:
+        [1] Wang, Z., Bovik, A. C., Sheikh, H. R., & Simoncelli, E. P. (2004). Image quality assessment: from error visibility to structural similarity. IEEE transactions on image processing, 13(4), 600-612.
+        [2] https://ece.uwaterloo.ca/~z70wang/research/ssim/.
+
+    """
+
+    core = vs.get_core()
+    funcName = 'SSIM'
+    
+    if not isinstance(clip1, vs.VideoNode):
+        raise TypeError(funcName + ': \"clip1\" must be a clip!')
+    if not isinstance(clip2, vs.VideoNode):
+        raise TypeError(funcName + ': \"clip2\" must be a clip!')
+    
+    if clip1.format.id != clip2.format.id:
+        raise ValueError(funcName + ': \"clip1\" and \"clip2\" must be of the same format!')
+    if clip1.width != clip2.width or clip1.height != clip2.height:
+        raise ValueError(funcName + ': \"clip1\" and \"clip2\" must be of the same width and height!')
+
+    c1 = (k1 * dynamic_range) ** 2
+    c2 = (k2 * dynamic_range) ** 2
+
+    if fun is None:
+        fun = functools.partial(core.tcanny.TCanny, sigma=1.5, mode=-1)
+    elif isinstance(fun, (int, float)):
+        fun = functools.partial(core.tcanny.TCanny, sigma=fun, mode=-1)
+    elif not callable(fun):
+        raise TypeError(funcName + ': \"fun\" must be a function or a float!')
+
+    # Store the "clip1"
+    clip1_src = clip1
+
+    # Convert to float type grayscale image
+    clip1 = mvf.GetPlane(clip1, plane)
+    clip2 = mvf.GetPlane(clip2, plane)
+    clip1 = mvf.Depth(clip1, depth=32, sample=vs.FLOAT, **depth_args)
+    clip2 = mvf.Depth(clip2, depth=32, sample=vs.FLOAT, **depth_args)
+
+    # Filtered by a 2x2 average filter and then down-sampled by a factor of 2
+    if downsample:
+        clip1 = IQA_downsample(clip1)
+        clip2 = IQA_downsample(clip2)
+
+    # Core algorithm
+    mu1 = fun(clip1)
+    mu2 = fun(clip2)
+    mu1_sq = core.std.Expr([mu1], ['x dup *'])
+    mu2_sq = core.std.Expr([mu2], ['x dup *'])
+    mu1_mu2 = core.std.Expr([mu1, mu2], ['x y *'])
+    sigma1_sq_pls_mu1_sq = fun(core.std.Expr([clip1], ['x dup *']))
+    sigma2_sq_pls_mu2_sq = fun(core.std.Expr([clip2], ['x dup *']))
+    sigma12_pls_mu1_mu2 = fun(core.std.Expr([clip1, clip2], ['x y *']))
+
+    if c1 > 0 and c2 > 0:
+        expr = '2 x * {c1} + 2 y x - * {c2} + * z a + {c1} + b c - d e - + {c2} + * /'.format(c1=c1, c2=c2)
+        expr_clips = [mu1_mu2, sigma12_pls_mu1_mu2, mu1_sq, mu2_sq, sigma1_sq_pls_mu1_sq, mu1_sq, sigma2_sq_pls_mu2_sq, mu2_sq]
+        ssim_map = core.std.Expr(expr_clips, [expr])
+    else:
+        denominator1 = core.std.Expr([mu1_sq, mu2_sq], ['x y + {c1} +'.format(c1=c1)])
+        denominator2 = core.std.Expr([sigma1_sq_pls_mu1_sq, mu1_sq, sigma2_sq_pls_mu2_sq, mu2_sq], ['x y - z a - + {c2} +'.format(c2=c2)])
+
+        numerator1_expr = '2 z * {c1} +'.format(c1=c1)
+        numerator2_expr = '2 a z - * {c2} +'.format(c2=c2)
+        expr = 'x y * 0 > {numerator1} {numerator2} * x y * / x 0 = not y 0 = and {numerator1} x / {true} ? ?'.format(numerator1=numerator1_expr, numerator2=numerator2_expr, true=1)
+        ssim_map = core.std.Expr([denominator1, denominator2, mu1_mu2, sigma12_pls_mu1_mu2], [expr])
+
+    # The following code is modified from mvf.PlaneStatistics(), which is used to compute the mean of the SSIM index map as MSSIM
+    if core.std.get_functions().__contains__('PlaneStats'):
+        map_mean = core.std.PlaneStats(ssim_map, plane=[0], prop='PlaneStats')
+    else:
+        map_mean = core.std.PlaneAverage(ssim_map, plane=[0], prop='PlaneStatsAverage')
+
+    def _PlaneSSIMTransfer(n, f):
+        fout = f[0].copy()
+        fout.props.PlaneSSIM = f[1].props.PlaneStatsAverage
+        return fout
+    output_clip = ssim_map if show_map else clip1_src
+    output_clip = core.std.ModifyFrame(output_clip, [output_clip, map_mean], selector=_PlaneSSIMTransfer)
 
     return output_clip
 
