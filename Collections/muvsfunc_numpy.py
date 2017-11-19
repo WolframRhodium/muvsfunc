a723ef2a904a20823934701cfd50a6ef9d39fb55
WolframRhodium <WolframRhodium@users.noreply.github.com>
2017-11-19 23:58:34 +0800

    Add L0GradientProjection()

--- Collections/muvsfunc_numpy.py
+++ Collections/muvsfunc_numpy.py
@@ -1,11 +1,13 @@
 """
 VapourSynth functions:
-    numpy_process
+    numpy_process (helper function)
     L0Smooth
+    L0GradientProjection
 
 NumPy functions:
     L0Smooth_core
     psf2otf
+    L0GradProj_core
 """
 
 import functools
@@ -15,7 +17,7 @@ import mvsfunc as mvf
 import numpy as np
 
 def numpy_process(clip, numpy_function, per_plane=True, lock_source_array=True, **fun_args):
-    """Helper function for filtering clip in numpy
+    """Helper function for filtering clip in NumPy
 
     Args:
         clip: Input cilp.
@@ -75,6 +77,8 @@ def numpy_process(clip, numpy_function, per_plane=True, lock_source_array=True, 
 def L0Smooth(clip, lamda=2e-2, kappa=2, color=True, **depth_args):
     """L0 Smooth in VapourSynth
 
+    The official name is "L0 Gradient Minimization".
+
     L0 smooth is a new image editing method, particularly effective for sharpening major edges
     by increasing the steepness of transitions while eliminating a manageable degree of low-amplitude structures.
     It is achieved in an unconventional optimization framework making use of L0 gradient minimization,
@@ -85,7 +89,7 @@ def L0Smooth(clip, lamda=2e-2, kappa=2, color=True, **depth_args):
     All the internal calculations are done at 32-bit float.
 
     Args:
-        src: Input clip with no chroma subsampling.
+        clip: Input clip with no chroma subsampling.
 
         lamda: (float) Smoothing parameter controlling the degree of smooth.
             A large "lamda" makes the result have very few edges.
@@ -169,6 +173,8 @@ def L0Smooth_generate_denormin2(size2D):
 def L0Smooth_core(src, lamda=2e-2, kappa=2, Denormin2=None, copy=False):
     """L0 Smooth in NumPy.
 
+    The official name is "L0 Gradient Minimization".
+
     Args:
         src: 2-D or 3-D numpy array. The length along the second dimension must be even.
             3-D data will be processed collaboratively, which is the same as the official MATLAB version.
@@ -191,16 +197,16 @@ def L0Smooth_core(src, lamda=2e-2, kappa=2, Denormin2=None, copy=False):
 
     """
 
-    funcName = 'L0_smooth_core'
-
-    if copy:
-        src = src.copy()
+    funcName = 'L0Smooth_core'
 
     if not isinstance(src, np.ndarray) or src.ndim not in (2, 3):
         raise TypeError(funcName + ': \"src\" must be 2-D or 3-D numpy data!')
 
     if src.shape[1] & 1:
         raise TypeError(funcName + ': the length of \"src\" along the second dimension must be even!')
+
+    if copy:
+        src = src.copy()
 
     # Get size
     imgSize = src.shape
@@ -210,11 +216,7 @@ def L0Smooth_core(src, lamda=2e-2, kappa=2, Denormin2=None, copy=False):
 
     # Generate constant "Denormin2"
     if Denormin2 is None:
-        fx = np.array([[1, -1]])
-        fy = np.array([[1], [-1]])
-        otfFx = psf2otf(fx, outSize=size2D)
-        otfFy = psf2otf(fy, outSize=size2D)
-        Denormin2 = np.abs(otfFx) ** 2 + np.abs(otfFy) ** 2
+        Denormin2 = L0Smooth_generate_denormin2(size2D)
 
     if Denormin2.shape[:2] == size2D:
         Denormin2 = Denormin2[:, :size2D[1]//2+1]
@@ -320,3 +322,258 @@ def psf2otf(psf, outSize=None, fast=False):
             otf = np.real(otf)
 
     return otf
+
+
+def L0GradientProjection(clip, alpha=0.08, precision=255, epsilon=0.0002, maxiter=5000, gamma=3, eta=0.95, color=True, **depth_args):
+    """L0 Gradient Projection in VapourSynth
+
+    L0 gradient projection is a new edge-preserving filtering method based on the L0 gradient.
+    In contrast to the L0 gradient minimization,  L0 gradient projection framework is intuitive
+    because one can directly impose a desired L0 gradient value on the output image.
+    The constrained optimization problem is  minimizing the quadratic data-fidelity subject to the hard constraint that
+    the L0 gradient is less than a user-given parameter "alpha".
+    The solution is based on alternating direction method of multipliers (ADMM), while the hard constraint is handled by
+    projection onto the mixed L1,0 pseudo-norm ball with variable splitting, and the computational complexity is O(NlogN).
+
+    However, current implementation here is extremely slow. In my experiment, the number of iteration of this algorithm is far more than L0Smooth.
+
+    All the internal calculations are done at 32-bit float.
+
+    Args:
+        clip: Input clip with no chroma subsampling.
+
+        alpha: (float) L0 gradient of output in percentage form, i.e. the range is [0, 1].
+            It can be seen as the degree of flatness in the output.
+            Default is 0.08.
+
+        precision: (float) Precision of the calculation of L0 gradient. The larger the value, the more accurate the calculation.
+            Default is 255.
+
+        epsilon: (float) Stopping criterion in percentage form, i.e. the range is [0, 1].
+            It determines the allowable error from alpha.
+            Default is 0.0002.
+
+        maxiter: (int) Maximum number of iterations.
+            Default is 5000.
+
+        gamma: (int) Step size of ADMM.
+            Default is 3.
+
+        eta: (int) Controling gamma for nonconvex optimization.
+            It stabilizes ADMM for nonconvex optimization.
+            According to recent convergence analyses of ADMM for nonconvex cases, under appropriate conditions,
+            the sequence generated by ADMM converges to a stationary point with sufficiently small gamma.
+            Default is 0.95.
+
+        depth_args: (dict) Additional arguments passed to mvf.Depth() in the form of keyword arguments.
+            Default is {}.
+
+    Ref:
+        [1] Ono, S. (2017). $ L_ {0} $ Gradient Projection. IEEE Transactions on Image Processing, 26(4), 1554-1564.
+        [2] Ono, S. (2017, March). Edge-preserving filtering by projection onto L 0 gradient constraint. In Acoustics, Speech and Signal Processing (ICASSP), 2017 IEEE International Conference on (pp. 1492-1496). IEEE.
+        [3] https://sites.google.com/site/thunsukeono/publications
+
+    TODO: Optimize FFT using pyfftw library.
+
+    """
+
+    funcName = 'L0GradientProjection'
+
+    core = vs.get_core()
+
+    if not isinstance(clip, vs.VideoNode) or any((clip.format.subsampling_w, clip.format.subsampling_h)):
+        raise TypeError(funcName + ': \"clip\" must be a clip with no chroma subsampling!')
+
+    # Internal parameters
+    bits = clip.format.bits_per_sample
+    sampleType = clip.format.sample_type
+    per_plane = not color or clip.format.num_planes == 1
+
+    # Convert to floating point
+    clip = mvf.Depth(clip, depth=32, sample=vs.FLOAT, **depth_args)
+
+    # Add padding for real Fast Fourier Transform
+    if clip.width & 1:
+        pad = True
+        clip = core.std.AddBorders(clip, left=1)
+    else:
+        pad = False
+
+    # Pre-calculate constant 2-D matrix
+    size2D = (clip.height, clip.width)
+    Lap = L0GradProj_generate_Lap(size2D)
+
+    # Process
+    clip = numpy_process(clip, functools.partial(L0GradProj_core, alpha=alpha, precision=precision, epsilon=epsilon, maxiter=maxiter,
+        gamma=gamma, eta=eta, Lap=Lap, copy=True), per_plane=per_plane)
+
+    # Crop the padding
+    if pad:
+        clip = core.std.CropRel(clip, left=1)
+
+    # Convert the bit depth and sample type of output to the same as input
+    clip = mvf.Depth(clip, depth=bits, sample=sampleType, **depth_args)
+
+    return clip
+
+
+def L0GradProj_core(src, alpha=0.08, precision=255, epsilon=0.0002, maxiter=5000, gamma=3, eta=0.95, Lap=None, copy=False):
+    """L0 Gradient Projection in NumPy.
+
+    Args:
+        src: 2-D or 3-D numpy array. The length along the second dimension must be even.
+            3-D data will be processed collaboratively, which is the same as the official MATLAB version.
+
+        alpha: (float) L0 gradient of output in percentage form, i.e. the range is [0, 1]. Default is 0.08.
+
+        precision: (float) Precision of the calculation of L0 gradient. Default is 255.
+
+        epsilon: (float) Stopping criterion in percentage form, i.e. the range is [0, 1]. Default is 0.0002.
+
+        maxiter: (int) Maximum number of iterations. Default is 5000.
+
+        gamma: (int) Step size of ADMM. Default is 3.
+
+        eta: (int) Controling gamma for nonconvex optimization. Default is 0.95.
+
+        Lap: (ndarray) Constant matrix. If it is None, it will be calculated automatically.
+
+        copy: (bool) Whether to copy the data before processing. Default is False.
+
+        For detailed documentation, please refer to the documentation of "L0GradientProjection" funcion in current library.
+
+    TODO: Optimize FFT using pyfftw library.
+
+    """
+
+    funcName = 'L0GradProj_core'
+
+    if not isinstance(src, np.ndarray) or src.ndim not in (2, 3):
+        raise TypeError(funcName + ': \"src\" must be 2-D or 3-D numpy data!')
+
+    if src.shape[1] & 1:
+        raise TypeError(funcName + ': the length of \"src\" along the second dimension must be even!')
+
+    if copy:
+        src = src.copy()
+
+    src_ndim = src.ndim
+    src_shape = src.shape
+
+
+    N = src_shape[0] * src_shape[1]
+    alpha = round(alpha * N)
+    epsilon *= N
+
+    if src_ndim == 2:
+        src = src[:, :, np.newaxis, np.newaxis]
+    else: # img.ndim == 3
+        src = src[:, :, :, np.newaxis]
+
+    # difference operators (periodic boundary)
+    D = lambda x: np.concatenate([x[np.r_[1:x.shape[0], 0], :, :, :] - x, x[:, np.r_[1:x.shape[1], 0], :, :] - x], axis=3)
+    Dt = lambda x: np.vstack([-x[:1, :, :, :1] + x[-1:, :, :, :1], -x[1:, :, :, :1] + x[:-1, :, :, :1]]) + np.hstack([-x[:, :1, :, 1:2] + x[:, -1:, :, 1:2], -x[:, 1:, :, 1:2] + x[:, :-1, :, 1:2]])
+
+    # for fftbased diagonilization
+    if Lap is None:
+        Lap = L0GradProj_generate_Lap(src_shape[:2])
+
+    if Lap.shape == src_shape[:2]:
+        Lap = Lap[:, :, np.newaxis, np.newaxis]
+
+    if Lap.shape != (*src_shape[:2], 1, 1):
+        raise ValueError(funcName + ': the shape of \"Lap\" must be {}!'.format(src_shape[:2]))
+
+    # calculating L0 gradient value
+    def L0gradcalc(z):
+        # z: 3-D array
+        # return L0GradValue(D((z[:, :, :, np.newaxis] * 255).astype(np.uint8).astype(np.float32)))
+        return L0GradProj_L0GradValue(D(np.round(z[:, :, :, np.newaxis] * precision)))
+
+    # variables
+    u = np.empty_like(src)
+    v = D(src)
+    w = v.copy()
+
+    for i in range(maxiter):
+        rhs = src + Dt(v - w) / gamma
+
+        u[:] = np.real(np.fft.ifft2(np.fft.fft2(rhs, axes=(0, 1)) / (Lap / gamma + 1), axes=(0, 1)))
+        v[:] = L0GradProj_ProjL1ball(D(u) + w, alpha)
+        w += D(u) - v
+
+        gamma *= eta
+
+        L0Grad = L0gradcalc(u)
+        if abs(L0Grad - alpha) < epsilon:
+            break
+
+    u = u.reshape(src_shape)
+
+    return u
+
+
+def L0GradProj_ProjL1ball(Du, alpha):
+    """Internal function for L0GradProj_core()
+
+    Projection onto mixed L1,0 pseudo-norm ball with binary mask
+
+    Args:
+        Du: 4-D array
+        alpha: (int) Threshold of the constraint.
+
+    """
+
+    sizeDu = Du.shape
+
+    Du1 = Du[-1, :, :, 0].copy()
+    Du2 = Du[:, -1, :, 1].copy()
+
+    # masking differences between opposite boundaries
+    Du[-1, :, :, 0] = 0
+    Du[:, -1, :, 1] = 0
+
+    sumDu = np.sum(Du ** 2, axis=(2, 3))
+    # The worst-case complexity of Sort(modified quicksort actually) in MATLAB is O(n^2)
+    # while it is O(n) for numpy.partition(introselect)
+    I = np.argpartition(-sumDu.reshape(-1), alpha)[:alpha]
+    threInd = np.zeros(sizeDu[:2])
+    threInd.reshape(-1)[I] = 1; # set ones for values to be held
+
+    threInd = np.tile(threInd[:, :, np.newaxis, np.newaxis], (1, 1, *sizeDu[2:]))
+    Du *= threInd
+
+    Du[-1, :, :, 0] = Du1
+    Du[:, -1, :, 1] = Du2
+
+    return Du
+
+
+def L0GradProj_L0GradValue(Du):
+    """Internal function for L0GradProj_core()
+
+    Calculate L0 gradient
+
+    Args:
+        Du: 4-D array
+
+    """
+
+    Du[-1, :, :, 0] = 0
+    Du[:, -1, :, 1] = 0
+
+    return np.count_nonzero(np.abs(Du).sum(axis=(2, 3)).round())
+
+
+def L0GradProj_generate_Lap(size2D):
+    """Helper function to generate constant "Denormin2"
+    """
+    Lap = np.zeros(size2D)
+    Lap[0, 0] = 4
+    Lap[0, 1] = -1
+    Lap[1, 0] = -1
+    Lap[-1, 0] = -1
+    Lap[0, -1] = -1
+    Lap = np.fft.fft2(Lap, axes=(0, 1))
+
+    return Lap
