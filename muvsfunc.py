WolframRhodium/muvsfunc

commit 4d955b62bdfb3dc9d77b32f8d3310e5482b913fc

getnative(): optimize plot

muvsfunc.py
@@ -6061,13 +6061,35 @@ def getnative(clip: vs.VideoNode, rescalers: Union[rescale.Rescaler, List[rescal
                 else:
                     ticks.append(height)
             return ticks
-
+        def get_kernel_ticks(data: Sequence[float], kernels: List[str]) -> tuple[List[int], List[str]]:
+            interval = round(len(kernels) * 0.05)
+            log10_data = [math.log10(v) for v in data]
+            d2_log10_data = []
+            valley_kernels = []
+            for i in range(1, len(data) - 1):
+                if log10_data[i - 1] > log10_data[i] and log10_data[i + 1] > log10_data[i]:
+                    d2_log10_data.append(log10_data[i - 1] + log10_data[i + 1] - 2 * log10_data[i])
+                    valley_kernels.append(kernels[i])
+            candidate_kernels = [valley_kernels[i] for _, i in sorted(zip(d2_log10_data, range(len(valley_kernels))), reverse=True)]
+            candidate_kernels.append(kernels[0])
+            candidate_kernels.append(kernels[-1])
+            ticks = []
+            ticklabels = []
+            for kernel in candidate_kernels:
+                for ticklabel in ticklabels:
+                    if abs(kernels.index(kernel) - kernels.index(ticklabel)) < interval:
+                        break
+                else:
+                    ticks.append(kernels.index(kernel))
+                    ticklabels.append(kernel)
+            return ticks, ticklabels
         if dark:
             plt.style.use("dark_background")
             fmt = ".w-"
         else:
             fmt = ".-"
         fig, ax = plt.subplots(figsize=(12, 8))
+        fig.set_tight_layout(True)
         if mode == Mode.MULTI_FRAME:
             save_filename = get_save_filename(f"verify_{rescalers[0].name}_{src_heights[0]}")
             ax.plot(range(len(data)), data, fmt)
@@ -6088,10 +6110,14 @@ def getnative(clip: vs.VideoNode, rescalers: Union[rescale.Rescaler, List[rescal
             ax.plot(range(len(data)), data, fmt)
             ticks = list(range(len(data)))
             ticklabels = [rescaler.name for rescaler in rescalers]
+            rescaler_names = ticklabels
+            if len(data) >= 20:
+                ticks, ticklabels = get_kernel_ticks(data, rescaler_names)
+            ticklabels = [ticklabel.replace('_', '\n') for ticklabel in ticklabels]
             ax.set(xlabel="Kernel", xticks=ticks, xticklabels=ticklabels, ylabel="Relative error", title=save_filename, yscale="log")
             with open(f"{save_filename}.txt", "w") as ftxt:
                 import pprint
-                pprint.pprint(list(zip(ticklabels, data)), stream=ftxt)
+                pprint.pprint(list(zip(rescaler_names, data)), stream=ftxt)
         fig.savefig(f"{save_filename}")
         plt.close()
 

commit ec233aaff93b80bc6da67b40476823518cde1030

getnative(): fix a bug introduced in 4d955b6

muvsfunc.py
@@ -6110,11 +6110,11 @@ def getnative(clip: vs.VideoNode, rescalers: Union[rescale.Rescaler, List[rescal
             ax.plot(range(len(data)), data, fmt)
             ticks = list(range(len(data)))
             ticklabels = [rescaler.name for rescaler in rescalers]
-            if len(data) >= 20:
-                ticks, ticklabels = get_kernel_ticks(data, ticklabels)
             with open(f"{save_filename}.txt", "w") as ftxt:
                 import pprint
                 pprint.pprint(list(zip(ticklabels, data)), stream=ftxt)
+            if len(data) >= 20:
+                ticks, ticklabels = get_kernel_ticks(data, ticklabels)
             ticklabels = [ticklabel.replace('_', '\n') for ticklabel in ticklabels]
             ax.set(xlabel="Kernel", xticks=ticks, xticklabels=ticklabels, ylabel="Relative error", title=save_filename, yscale="log")
         fig.savefig(f"{save_filename}")